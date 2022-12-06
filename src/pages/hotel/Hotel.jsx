dcbb41ec02af463501ada1aff927e34d0f712b13
smug <supersarath007@gmail.com>
2022-12-06 12:37:47 +0530

    Reacted

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -44,15 +44,15 @@ const Hotel = () => {
 
   const handleMove = (direction) => {
     let newSlideNumber;
-
     if (direction === "l") {
-      newSlideNumber = slideNumber === 0 ? 5 : slideNumber - 1;
+      newSlideNumber = slideNumber === 0 ? 5 : slideNumber - 1
     } else {
+
       newSlideNumber = slideNumber === 5 ? 0 : slideNumber + 1;
     }
+    setSlideNumber(newSlideNumber);
+  }
 
-    setSlideNumber(newSlideNumber)
-  };
 
   return (
     <div>
